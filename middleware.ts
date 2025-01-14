a238af3b424581e6be6b89388fec647ae826e2c5
AMRENE5435 <nihal.amrene@edu.uiz.ac.ma>
2025-01-14 20:36:01 +0100

    fav iso , next seo

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
-import createMiddleware from 'next-intl/middleware'
-import { routing } from './i18n/routing'
+import createMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
+import NextAuth from 'next-auth';
+import authConfig from './auth.config';
 
-import NextAuth from 'next-auth'
-import authConfig from './auth.config'
-
+// Pages accessibles sans authentification
 const publicPages = [
   '/',
   '/search',
@@ -13,42 +13,46 @@ const publicPages = [
   '/cart/(.*)',
   '/product/(.*)',
   '/page/(.*)',
-  // (/secret requires auth)
-]
+];
 
-const intlMiddleware = createMiddleware(routing)
-const { auth } = NextAuth(authConfig)
+// Middleware pour next-intl
+const intlMiddleware = createMiddleware(routing);
+
+// Middleware pour NextAuth
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  // Vérifie si la page est publique
   const publicPathnameRegex = RegExp(
     `^(/(${routing.locales.join('|')}))?(${publicPages
       .flatMap((p) => (p === '/' ? ['', '/'] : p))
       .join('|')})/?$`,
     'i'
-  )
-  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname)
+  );
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
 
-  const isProductPage = req.nextUrl.pathname.startsWith('/product')
-
-  if (isPublicPage || isProductPage) {
-    // return NextResponse.next()
-    return intlMiddleware(req)
+  if (isPublicPage) {
+    // Applique le middleware next-intl pour les pages publiques
+    return intlMiddleware(req);
   } else {
+    // Vérifie si l'utilisateur est authentifié
     if (!req.auth) {
+      // Redirige vers la page de connexion avec un callbackUrl
       const newUrl = new URL(
         `/sign-in?callbackUrl=${
           encodeURIComponent(req.nextUrl.pathname) || '/'
         }`,
         req.nextUrl.origin
-      )
-      return Response.redirect(newUrl)
+      );
+      return Response.redirect(newUrl);
     } else {
-      return intlMiddleware(req)
+      // Applique le middleware next-intl pour les pages privées
+      return intlMiddleware(req);
     }
   }
-})
+});
 
 export const config = {
-  // Skip all paths that should not be internationalized
-  matcher: ['/((?!api|_next|favicon.ico|logo.png|.*\\..*).*)'],
-}
+  // Exclut les routes API, les fichiers statiques, et les fichiers Next.js
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)'],
+};
