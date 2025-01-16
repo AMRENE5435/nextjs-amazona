edd3fe0f2033d13a72cace5d1d90965742d48895
AMRENE5435 <nihal.amrene@edu.uiz.ac.ma>
2025-01-16 20:50:02 +0100

    bing next indexing

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+    titleTemplate: '%s | Laptop Solution',
+    defaultTitle: 'Laptop Solution',
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: 'https://www.laptopsolution.tech',
+      site_name: 'Laptop Solution',
+    },
+    twitter: {
+      handle: '@laptopsolution',
+      site: '@laptopsolution',
+      cardType: 'summary_large_image',
+    },
+  };
