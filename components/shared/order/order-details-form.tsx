fc3a71297a5d42e9a81825f9095ce1fbc1e5477d
AMRENE5435 <nihal.amrene@edu.uiz.ac.ma>
2025-01-23 04:24:48 +0100

    IMAGE TO IMG div issue corrected

--- components/shared/order/order-details-form.tsx
+++ components/shared/order/order-details-form.tsx
@@ -63,7 +63,6 @@ export default function OrderDetailsForm({
               </Badge>
             ) : (
               <div>
-                {' '}
                 <Badge variant='destructive'>Not delivered</Badge>
                 <div>
                   Expected delivery at{' '}
