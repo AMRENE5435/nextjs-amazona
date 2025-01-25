c0a852970e1c2f2bcf6ba3a8a3769946e86ff5f3
AMRENE5435 <nihal.amrene@edu.uiz.ac.ma>
2025-01-25 01:14:40 +0100

    google tag

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -60,6 +60,19 @@ export default async function AppLayout({
       suppressHydrationWarning
     >
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T2CRZFXS');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel='icon' href='/favicon.ico' />
         <title>{setting.site.name}</title>
         <meta name='description' content={setting.site.description} />
@@ -99,6 +112,16 @@ export default async function AppLayout({
       <body
         className={`min-h-screen ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-T2CRZFXS'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <NextIntlClientProvider locale={locale} messages={messages}>
           <ClientProviders setting={{ ...setting, currency }}>
             {children}
